0747a377a39e7788db24816de3de628bac47ed8b
Caio Willian <caiow.willian@gmail.com>
2021-08-02 23:37:11 -0300

    chore: initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 0; i < q.Length; i++)
+            {
+                q[i] = i;
+            }
+
+           int t = GetNumber(50);
+           Console.WriteLine(t);
+
+        }
+        static int[] q = new int[20];
+        public static int GetNumber(int targetValue)
+        {
+            var min = 0;
+            var max = q.Length - 1;
+            int guess = 0;
+            var counter = 0;
+
+             while (max >= min) {
+                counter++;
+                guess = (int)Math.Floor((decimal)(max+min)/2);
+                if (q[guess] == targetValue) {
+                    return guess;
+                } else if (q[guess] < targetValue) {
+                    min = guess + 1;
+                } else {
+                    max = guess - 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
